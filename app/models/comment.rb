a2325dfade9c1c6b098cf6359014fe3ccda3ece9
1000miles <1000miles@users.noreply.github.com>
2015-07-23 16:01:00 +0200

    add changes to email view with haml

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -52,12 +52,8 @@ class Comment < ActiveRecord::Base
   end
 
   private
-<<<<<<< HEAD
 
-=======
-   
   # Send notification email about a comment in an event
->>>>>>> 6d151dd... create mail view with haml code
   def send_notification
     Mailbot.delay.send_notification_email_for_comment(self)
   end
