3eb2080c43c2258170d51cfc860f8adc64a9ec17
1000miles <1000miles@users.noreply.github.com>
2015-08-12 18:58:46 +0200

    add datatables for comments page

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,13 @@
 class Admin::CommentsController < Admin::BaseController
   load_and_authorize_resource
-  
+
   def index
     @comments = Comment.all
+  end
+
+  def self.unread
+  end
+
+  def posted
   end
 end
