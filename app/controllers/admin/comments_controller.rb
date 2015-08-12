25dfb8d6297a95c9bc62458e1d441b252f2a0ff0
1000miles <1000miles@users.noreply.github.com>
2015-08-12 18:58:46 +0200

    add datatables for comments page

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -28,9 +28,15 @@ module Admin
 =======
 class Admin::CommentsController < Admin::BaseController
   load_and_authorize_resource
-  
+
   def index
     @comments = Comment.all
 >>>>>>> 9f12201... create comments page & add routes
+  end
+
+  def self.unread
+  end
+
+  def posted
   end
 end
--- app/helpers/admin/comments_helper.rb
+++ app/helpers/admin/comments_helper.rb
@@ -1,5 +1,5 @@
 module Admin::CommentsHelper
-  def unread_comments
+  def unread_comments #dropdown-toggle
     Comment.limit(5).order(created_at: :desc)
   end
 end
